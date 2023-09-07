fbc637a7b82da664fd8fa96b17887f03ab3e9b21
Binyamin Y Cohen <binyamincohen555@gmail.com>
2023-09-08 01:59:50 +0300

    Add span information to `TokenInfo`

--- zs/text/token_info.py
+++ zs/text/token_info.py
@@ -2,9 +2,26 @@ __all__ = [
     "TokenInfo",
 ]
 
+import dataclasses
 
 from utilz.debug.file_info import Span
+
+
+@dataclasses.dataclass(
+    slots=True,
+    frozen=True,
+    init=False
+)
 class TokenInfo:
+    @property
+    def span(self):
+        spans = []
+        for field in dataclasses.fields(self):
+            value = getattr(self, field.name)
+            if value is not None:
+                spans.append(value.span)
+        return Span.combine(*spans)
+
     def __str__(self):
         try:
             return str(getattr(self, self.__slots__[0]))
